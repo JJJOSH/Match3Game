9c74aef67c6deefa4db7ed3a6486215cacf2c735
unknown <JJJOSH>
2021-10-18 09:22:18 +0100

    first commit

---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    private int x;
+    private int y;
+
+    public int X
+    {
+        get { return x; }
+        set
+        {
+            if (IsMovable())
+            {
+                x = value;
+            }
+        }
+    }
+
+    public int Y
+    {
+        get { return y; }
+        set
+        {
+            if (IsMovable())
+            {
+                y = value;
+            }
+        }
+    }
+
+    private BackGrid.PieceType type;
+
+    public BackGrid.PieceType Type
+    {
+        get { return type; }
+    }
+
+    private BackGrid grid;
+
+    public BackGrid GridRef
+    {
+        get { return grid; }
+    }
+
+    private MovablePiece movableComponent;
+
+    public MovablePiece MovableComponent
+    {
+        get { return movableComponent; }
+    }
+
+    private ColorPiece colorComponent;
+
+    public ColorPiece ColorComponent
+    {
+        get { return colorComponent; }
+    }
+
+    void Awake()
+    {
+        movableComponent = GetComponent<MovablePiece>();
+        colorComponent = GetComponent<ColorPiece>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init(int _x, int _y, BackGrid _grid, BackGrid.PieceType _type)
+    {
+        x = _x;
+        y = _y;
+        grid = _grid;
+        type = _type;
+    }
+
+    public bool IsMovable()
+    {
+        return movableComponent != null;
+    }
+
+    public bool IsColored()
+    {
+        return colorComponent != null;
+    }
+}
